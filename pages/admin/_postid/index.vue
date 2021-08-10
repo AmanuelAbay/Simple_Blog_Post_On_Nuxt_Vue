54f634657dda0a6d617b205e5418e841c02ba1c4
AmanuelAbay <amanab.449@gmail.com>
2021-08-10 19:46:24 +0300

    Simple Blog Post on Nuxt Vue

---
+++ pages/admin/_postid/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+    layout:'admin',
+    components:{
+        AdminPostForm
+    },
+    data(){
+        return{
+            loadedPost:{
+                author:'Amanuel Abay',
+                title:'Post Test',
+                content:'The first loaded post content',
+                thumbnailLink:'https://www.agilitypr.com/wp-content/uploads/2020/02/technology-1-1.jpg'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.update-form{
+    width:90%;
+    margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+    .update-form{
+        width: 500px;
+    }
+}
+</style>
